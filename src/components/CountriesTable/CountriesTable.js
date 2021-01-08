fcafcdb313b0b99938be1b1c6cb41718d3644d3e
andri715 <76740718+andri715@users.noreply.github.com>
2021-01-08 20:13:28 +0700

    Add files via upload

---
+++ src/components/CountriesTable/CountriesTable.js
@@ -0,0 +1,62 @@
+
+import { KeyboardArrowDownRounded } from "@material-ui/icons";
+import { KeyboardArrowUpRounded } from "@material-ui/icons";
+import styles from "./CountriesTable.module.css";
+
+const orderBy = (countries, direction) => {
+    if (direction === "asc") {
+    return 
+    [...countries].sort((a,b) => (a.population > b.population ? 1: -1));
+    }
+
+    if (direction === "desc") {
+    return [...countries].sort((a, b) => (a.population > b.population ? -1: 1));    
+    }
+
+    return countries;
+};
+
+const SortArrow =({ direction}) =>  {
+    if (!direction){
+        return <></>;
+    }
+    if (direction == "desc") {
+        return (
+            <div className={styles.heading_arrow}>
+                <KeyboardArrowDownRounded color="inherit" />
+            </div>
+        );
+    } else {
+        return (
+        <div className={styles.heading_arrow}>
+                <KeyboardArrowUpRounded color="inherit" />
+            </div>
+            );
+    }
+
+};
+const CountriesTable = ({ countries }) => {
+    const orderedCountries = orderBy(countries, "desc");
+    return (
+    <div>
+     <div className={styles.heading}>
+         <button className={styles.heading_name}>
+             <div>Name</div>
+            <SortArrow />
+         </button>
+         <button className={styles.heading_population}>
+             <div>Population</div>
+
+             <SortArrow direction="asc"/>
+         </button>
+     </div>
+     {orderedCountries.map((country) => <div className={styles.row}>
+         <div className={styles.name}>{country.name}</div>
+         <div className={styles.population}>{country.population}</div>
+     </div>)}
+     </div>
+
+    );
+    };
+
+export default CountriesTable
---
+++ src/pages/index.js
@@ -0,0 +1,27 @@
+import SearchInput from "../components/Searchinput/Searchinput";
+import Layout from "../components/Layout/Layout";
+import styles from "../styles/Home.module.css";
+import CountriesTable from "../components/CountriesTable/CountriesTable";
+
+export default function Home({countries}) {
+  console.log(countries);
+  return (
+  <Layout>
+    <div className={styles.counts}>Found {countries.length}countries
+    </div>
+    <SearchInput placeholder="Filter by Name, Region or SubRegion"/>
+    <CountriesTable countries={countries}/>
+  </Layout>
+  );
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://restcountries.eu/rest/v2/all");
+  const countries =await res.json();
+  return {
+    props: {
+      countries,
+
+    },
+  };
+};
